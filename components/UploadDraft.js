0315d17ba84dfb6995d0e49d59fd7be912077a92
RouteExplorer <75504775+RouteExplorer@users.noreply.github.com>
2022-03-16 13:32:19 +0000

    ran 'standard --fix'

--- components/UploadDraft.js
+++ components/UploadDraft.js
@@ -1,53 +1,52 @@
-import React, { Component } from "react";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { Component } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 class UploadDraft {
-  async dateCheck() {
-    console.log("UPLOAD CHECKER WORKING!!!!!!");
+  async dateCheck () {
+    console.log('UPLOAD CHECKER WORKING!!!!!!')
 
-    const get = await AsyncStorage.getItem("draftStore");
-    const parsed = JSON.parse(get);
+    const get = await AsyncStorage.getItem('draftStore')
+    const parsed = JSON.parse(get)
 
-    const today = new Date();
+    const today = new Date()
 
     for (let i = 0; i < parsed.length; i++) {
       if (parsed[i].date != null && new Date(parsed[i].date) <= today) {
-        
-        const token = await AsyncStorage.getItem("@session_token");
-        const id = parsed[i].id;
+        const token = await AsyncStorage.getItem('@session_token')
+        const id = parsed[i].id
 
-        return fetch("http://localhost:3333/api/1.0.0/user/" + id + "/post", {
-          method: "POST",
+        return fetch('http://localhost:3333/api/1.0.0/user/' + id + '/post', {
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
-            "X-Authorization": token,
+            'Content-Type': 'application/json',
+            'X-Authorization': token
           },
           body: JSON.stringify({
-            text: parsed[i].text,
-          }),
+            text: parsed[i].text
+          })
         })
           .then((response) => {
             if (response.status === 200) {
-              return response.json();
+              return response.json()
             } else if (response.status === 401) {
-              return response.json();
+              return response.json()
             } else if (response.status === 201) {
               // Remove from item from full list then re-add into the storage
-              parsed.splice(i, 1);
-              AsyncStorage.setItem("draftStore", JSON.stringify(parsed));
+              parsed.splice(i, 1)
+              AsyncStorage.setItem('draftStore', JSON.stringify(parsed))
             } else {
-              throw "Something went wrong";
+              throw 'Something went wrong'
             }
           })
 
           .catch((error) => {
-            console.log(error);
-          });
+            console.log(error)
+          })
       }
     }
   }
 }
 
-const UploadDrafts = new UploadDraft();
+const UploadDrafts = new UploadDraft()
 
-export default UploadDrafts;
+export default UploadDrafts
